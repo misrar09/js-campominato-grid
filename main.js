48f4b4b8d96cf983eb3bcdb48de9e5c93f553a2c
misrar09 <misrar09@gmail.com>
2023-11-07 17:02:46 +0100

    added funtion

--- main.js
+++ main.js
@@ -6,33 +6,32 @@ const grid = document.getElementById("grid");
 // Showing the grid by clicking on the button
 
 
-
 const playButton = document.querySelector(".playBtn")
 
-let created = false;
-
-playButton.addEventListener("click", function () {
-
-
+//creating a function which will stop the code to be excuted on more than one click
+function onceClicked(){
 
     // creating the 100 boxes with the loop
     for (let i = 1; i < 101; i++) {
-
+    
         const squareDiv = document.createElement("div");
         squareDiv.classList.add("box");
         grid.appendChild(squareDiv);
-
+    
         squareDiv.innerHTML = `${[i]}`;
-
+    
         // create the click envet
         squareDiv.addEventListener("click", function () {
             squareDiv.style.backgroundColor = "blue"
             console.log("box number is:", i)
         })
-
+    
     }
+    
+    playButton.removeEventListener("click", onceClicked)
+}
 
-}, { once: true }); // this will limit the code execution only to the first click
+playButton.addEventListener("click", onceClicked) 
 
 
 
