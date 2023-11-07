610ef43a15595d75215eb14fcb9d8d2a356f0a56
misrar09 <misrar09@gmail.com>
2023-11-07 19:56:01 +0100

    refactoring

--- main.js
+++ main.js
@@ -5,36 +5,90 @@ const grid = document.getElementById("grid");
 
 // Showing the grid by clicking on the button
 
+const difficulty = document.querySelector("#difficulty");
 
 const playButton = document.querySelector(".playBtn")
 
 //creating a function which will stop the code to be excuted on more than one click
-function onceClicked(){
+function onceClicked() {
 
-    // creating the 100 boxes with the loop
-    for (let i = 1; i < 101; i++) {
-    
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add("box");
-        grid.appendChild(squareDiv);
-    
-        squareDiv.innerHTML = `${[i]}`;
-    
-        // create the click envet
-        squareDiv.addEventListener("click", function () {
-            squareDiv.style.backgroundColor = "blue"
-            console.log("box number is:", i)
-        })
-    
+    let difficultyLevel = difficulty.value;
+
+    if (difficultyLevel == "easy") {
+
+        for (let i = 1; i < 101; i++) {
+
+            createSquareDiv("box", i); // created a funciton and nested function and saved the following code lines
+
+            //const squareDiv = document.createElement("div");
+            //squareDiv.classList.add("box");
+            //grid.appendChild(squareDiv);
+            //squareDiv.innerHTML = `${[i]}`;
+
+            //squareDiv.style.backgroundColor = "blue"
+           // console.log("box number is:", i)
+
+
+        }
     }
-    
-    playButton.removeEventListener("click", onceClicked)
+
+    else if (difficultyLevel == "medium") {
+
+        for (let i = 1; i < 82; i++) {
+
+            createSquareDiv("boxMedium", i); // created a funciton and saved the following code lines
+            //const squareDiv = document.createElement("div");
+            //squareDiv.classList.add("box");
+            //grid.appendChild(squareDiv);
+            //squareDiv.innerHTML = `${[i]}`;
+
+            //squareDiv.style.backgroundColor = "blue"
+           // console.log("box number is:", i)
+
+
+        }
+
+
+    }
+
+    else if (difficultyLevel == "hard") {
+        for (let i = 1; i < 50; i++) {
+
+            createSquareDiv("boxHard", i) // created a funciton and saved the following code lines
+            //const squareDiv = document.createElement("div");
+            //squareDiv.classList.add("box");
+            //grid.appendChild(squareDiv);
+            //squareDiv.innerHTML = `${[i]}`;
+
+            //squareDiv.style.backgroundColor = "blue"
+           // console.log("box number is:", i)
+
+        }
+
+
+    }
+    playButton.removeEventListener("click", onceClicked);
 }
 
-playButton.addEventListener("click", onceClicked) 
+playButton.addEventListener("click", onceClicked);
 
+function squareDivOutput(squareDiv, i) {
+    squareDiv.style.backgroundColor = "blue";
+    console.log("box number is:", i);
 
+}
 
+function createSquareDiv(className, i) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add(className);
+    grid.appendChild(squareDiv);
+    squareDiv.innerHTML = `${[i]}`;
+    squareDiv.addEventListener("click", function () {
+        squareDivOutput(squareDiv, i); // created a funciton and saved the following code lines
+        /*                 squareDiv.style.backgroundColor = "blue"
+                        console.log("box number is:", i) */
 
-
+    });
+    return squareDiv; // Return the created div element
+}
 
